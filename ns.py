e5c88a8e22725fb937e022ad557778b7967abef0
Guillem Perez <guillem.perez@iesjoandaustria.org>
2020-05-25 08:35:03 +0000

    ex 2

--- ns.py
+++ ns.py
@@ -12,14 +12,16 @@ nom=input('arxiu a obrir: ')
 fitxer = open(path+'/'+nom,'r')
 fitxerr=fitxer.read()
 print (fitxerr)
-resposta=input('vols executar el contingut del arxiu?')
+resposta=input('vols executar el contingut del arxiu? ')
 if resposta == 'si':
   pro = fitxerr.split(' ')
   if pro[0]=='moure':
-    if os.path.exists(path+'/'+pro[2]):
+    if os.path.exists(path+'/'+pro[1]):
       os.replace(path+'/'+pro[1] , path+'/'+pro[2])
     else:
       print('no existeix')
+      
+      
   elif pro[0]=='borrar':
     os.remove(path+'/'+pro[1])
   elif pro[0]=='copiar':
